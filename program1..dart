216db5891efc22f1c3be647e7c764e25789d813c
Eugene Israel <eugeneisrael12@gmail.com>
2024-03-18 16:34:10 +0300

    Initial commut

--- program1..dart
+++ program1..dart
@@ -1,2 +1,24 @@
+/*
 ## Program 1: Display Personal Information
 Write a Dart program using variables to display your name, age, school, and a hobby. Print it in one logical sentence.
+*/
+
+
+import 'dart:io';
+
+void main() {
+  print("Enter your name: ");
+  var name = stdin.readLineSync()!;
+
+  print("Enter your age: ");
+  var age = int.parse(stdin.readLineSync()!);
+
+  print("Enter your school name: ");
+  var school = stdin.readLineSync()!;
+
+  print("Enter your favorite hobby: ");
+  var hobby = stdin.readLineSync()!;
+
+  // Print the information in a sentence
+  print("My name is $name, I am $age years old, I go to $school, and I enjoy $hobby.");
+}
