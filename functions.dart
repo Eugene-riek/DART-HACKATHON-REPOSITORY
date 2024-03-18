9ce5250fe2e62ca54ee3ab5a147abb5c30fd2777
Eugene Israel <eugeneisrael12@gmail.com>
2024-03-18 16:09:39 +0300

    Initial commit

--- functions.dart
+++ functions.dart
@@ -1,2 +1,39 @@
+/*
 ## Program 2: Perform Mathematical Operations with Functions
 Write a Dart program that performs two mathematical operations using functions.
+*/
+
+import 'dart:io';
+void main() {
+  double add(double num1, double num2) {
+    return num1 + num2;
+  }
+
+  double multiply(double num1, double num2) {
+    return num1 * num2;
+  }
+
+  double divide(double num1, double num2) {
+    return num1 / num2;
+  }
+
+  double subtract(double num1, double num2) {
+    return num1 - num2;
+  }
+
+  print ("Enter the first number: ");
+  double num1 = double.parse(stdin.readLineSync()!);
+
+  print ("Enter the second number: ");
+  double num2 = double.parse(stdin.readLineSync()!);
+
+  double sum = add(num1, num2);
+  double product = multiply(num1, num2);
+  double quotient = divide(num1, num2);
+  double difference = subtract(num1, num2);
+
+  print("The sum of $num1 and $num2 is: $sum");
+  print("The product of $num1 and $num2 is: $product");
+  print("The quotient of $num1 and $num2 is: $quotient");
+  print("The difference between $num1 and $num2 is: $difference");
+}
